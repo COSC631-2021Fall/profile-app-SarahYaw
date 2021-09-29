2e9c1bc5899e7a23d218b57b677454c08d307b22
SarahYaw <syaw@emich.edu>
2021-09-29 15:26:23 +0000

    Content added

---
+++ profile-app/src/components.js
@@ -0,0 +1,54 @@
+import './App.css';
+
+  function Footer(){
+    return(
+      <>
+        <div>
+            <p>Education</p>
+            <ul>
+                <li>Bachelor of Science in Computer Science, April 2020 - GPA: 3.74   <br/>Eastern Michigan University, Ypsilanti, Michigan</li>
+                <li>Master of Science, Computer Science - Expected December 2022   <br/>Eastern Michigan University, Ypsilanti, Michigan</li>
+            </ul>
+        </div>
+        <div>
+            <p>Programming Proficiencies</p>
+            <ul>
+                <li>Adobe Photoshop, Illustrator, Indesign, Dreamweaver: 6 years</li>
+                <li>Lucidpress: 4 years</li>
+                <li>Canva: 4 years</li>
+                <li>HTML &amp; CSS: 6 years</li>
+                <li>Java: 6 years</li>
+                <li>JS: 3 years</li>
+                <li>MySQL &amp; NoSql: 1.5 years</li>
+                <li>Php: 1.5 years</li>
+                <li>Python: 1.5 years</li>
+            </ul>
+        </div>
+        <div>
+            <p>Projects</p>
+            <ul>
+                <li><a href="https://github.com/SarahYaw/CyberSec-HW2">Develop an application which has the options: RSA, Diffie-Hellman, and Elliptic curve cryptography - In Progress</a></li>
+                <li><a href="https://github.com/SarahYaw/592-PKI">Rough PKI built for Cybersecurity class -. In Progress</a></li>
+                <li><a href="https://github.com/SarahYaw/PRNG">Here is a program of different PRNG algorithms researched and coded up for an Independent Study</a></li>
+                <li><a href="https://github.com/SarahYaw/CommNetProj4">Centralized Routing Via a Routing Matrix (Using Dijkstra)</a></li>
+                <li><a href="https://github.com/SarahYaw/OSProj5">Virtual Memory Simulation</a></li>
+                <li><a href="https://github.com/SarahYaw/OSProj4">OS simulator with I/O</a></li>
+                <li><a href="https://github.com/SarahYaw/CommNetProj3">Multithreaded client-server program with encryption</a></li>
+                <li><a href="https://github.com/SarahYaw/OSProj3">Simple scheduler simulator</a></li>
+                <li><a href="https://github.com/SarahYaw/CommNetProj2">Multithreaded client server project</a></li>
+                <li><a href="https://github.com/SarahYaw/OSProj2">Multithreading and synchronization project for Operating Systems class</a></li>
+            </ul>
+        </div>
+        <div>
+            <p>Previous Work</p>
+            <ul>
+                <li>Rock Solid Logistics - Brighton, MI - Web Design Intern - April, 2021 to August, 2021</li>
+                <li>Eastern Michigan University - Ypsilanti, MI - Graduate Assistant - September, 2020 to Present</li>
+                <li>Eastern Michigan University - Ypsilanti, MI - Teaching Assistant - January, 2020 to April, 2020</li>
+                <li>The Honors College at Eastern Michigan University - Ypsilanti, MI - Graphic Design Communications Student Fellow - September, 2017 to April, 2020</li>
+            </ul>
+        </div>
+    </>
+    );
+  }
+  export default Footer;
--- profile-app/src/index.js
+++ profile-app/src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {header, education, skills, projects, prevWork} from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Footer from './components';
+//import {education, skills, projects, prevWork} from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <header />
+    <App />
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-ReactDOM.render(
-  <React.StrictMode>
-    <education />
-  </React.StrictMode>,
-  document.getElementById('body')
-);
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <Footer />
+//  </React.StrictMode>,
+//  document.getElementById('footer')
+//);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
