3ba69c71f06d378db1846e04e19565d74ae095b8
SarahYaw <syaw@emich.edu>
2021-10-04 23:36:20 +0000

    refactor

--- profile-app/src/index.js
+++ profile-app/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from './components';
-//import {education, skills, projects, prevWork} from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
