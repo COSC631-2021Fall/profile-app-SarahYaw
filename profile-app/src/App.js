e98d8068d6f6759d5e059ee61e34f5b279ac80de
SarahYaw <syaw@emich.edu>
2021-10-05 20:57:36 +0000

    added theme toggle ability

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -1,43 +1,57 @@
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import picture from './assets/Sarah.png';
+import light from './assets/background.png';
+import dark from './assets/darkbackground.png';
+import React, { useState } from 'react';
 import * as body from './components';
 
+var imgThemeID=1;
+function changeBodyImg() {
+  document.getElementsByClassName("ThemeButton")
+  const imgTheme = ["url('"+light+"')","url('"+dark+"')"];  
+  document.body.style.backgroundImage=imgTheme[imgThemeID];
+  imgThemeID = ((imgThemeID+1)%2);
+
+}
+
 function App() {
-  const subtitle = {
-    color: "#0e4870",
-    backgroundColor: "#CEE5E4",
-    paddingBottom:"19px",
-    width:"100%",
-  };
   const NavBar = {
-    backgroundColor:"#0e4870",
+    backgroundColor:"#98ABC8",
     padding:0,
-    marginLeft:"-10px",
-    width:"102%",
+    marginLeft:"-4px",
+    width:"100.9%",
     display:"inline-block",
   };
   const NavLi = {
     display:"inline",
     float:"left",
-    paddingRight:"5%",
+    paddingRight:"1%",
     paddingLeft:"1%",
-    backgroundColor:"#0e4870",
     width:"auto",
   };
   const LinkSty = {
     display:"block",
     padding:"10px",
-    backgroundColor:"#0e4870",
-    width:"100%",
-    color:"#8f9cc6",
+    color:"#EDE2EE",
+    borderRight:"#EDE2EE solid 1px",
   };
+
+  const divThemes = ["#CEE5E4", "#0e4870"];
+  const [divThemeID, setdivThemeid] = useState(0);
+  const txtThemes = ["#0e4870", "#CEE5E4"];
+  const [txtThemeID, settxtThemeid] = useState(0);
+  const switchTheme = () => {
+    setdivThemeid((id) => (id+1)%2)
+    settxtThemeid((id) => (id+1)%2)
+    changeBodyImg();
+  }
   return (
-    <div className="App">
+    <div style={{backgroundColor:divThemes[divThemeID], color:txtThemes[txtThemeID]}}>
       <img src={picture} alt="Sarah"/>
       <br/>
       <header>Sarah Yaw</header>
-      <p style={subtitle}>Contact: syaw@emich.edu</p>
+      <p>Contact: syaw@emich.edu</p>
       
       <BrowserRouter>
         <ul style={NavBar}>
@@ -46,13 +60,17 @@ function App() {
           <li style={NavLi}><Link to="/projects" style={LinkSty}>Past Projects</Link></li>
           <li style={NavLi}><Link to="/prevWork" style={LinkSty}>Previous Work</Link></li>
           <li style={NavLi}><Link to="/all" style={LinkSty}>All</Link></li>
+          <li style={NavLi}><Link to="/" style={LinkSty}>Bio</Link></li>
+          <li style={NavLi}><button className="ThemeButton" onClick={switchTheme}>Toggle Theme</button></li>
         </ul><br/>
 
+        <Route exact path="/" component={body.intro} />
         <Route path="/education" component={body.education} />
         <Route path="/skills" component={body.skills} />
         <Route path="/projects" component={body.projects} />
         <Route path="/prevWork" component={body.prevWork} />
         <Route path="/all"> 
+          <body.intro/>
           <body.education/>
           <body.skills/>
           <body.projects/>
--- profile-app/src/components.js
+++ profile-app/src/components.js
@@ -1,54 +1,86 @@
 import './App.css';
 
-  function Footer(){
+export const education = () => {
+    const subheader = {
+        backgroundColor:"#98ABC8",
+        padding:"10px", 
+    };
     return(
-      <>
-        <div>
-            <p>Education</p>
-            <table>
-                <tr><td>Bachelor of Science in Computer Science</td><td>April 2020, GPA: 3.74</td></tr><tr><td>Eastern Michigan University</td><td>Ypsilanti, Michigan</td></tr>
-                <tr><td>Master of Science, Computer Science</td><td>Expected December 2022</td></tr><tr><td>Eastern Michigan University</td><td>Ypsilanti, Michigan</td></tr>
-            </table>
-        </div>
-        <div>
-            <p>Programming Proficiencies</p>
-            <ul>
-                <li>Adobe Photoshop, Illustrator, Indesign, Dreamweaver: 6 years</li>
-                <li>Lucidpress: 4 years</li>
-                <li>Canva: 4 years</li>
-                <li>HTML &amp; CSS: 6 years</li>
-                <li>Java: 6 years</li>
-                <li>JS: 3 years</li>
-                <li>MySQL &amp; NoSql: 1.5 years</li>
-                <li>Php: 1.5 years</li>
-                <li>Python: 1.5 years</li>
-            </ul>
-        </div>
-        <div>
-            <p>Projects</p>
-            <ul>
-                <li><a href="https://github.com/SarahYaw/CyberSec-HW2">Develop an application which has the options: RSA, Diffie-Hellman, and Elliptic curve cryptography - In Progress</a></li>
-                <li><a href="https://github.com/SarahYaw/592-PKI">Rough PKI built for Cybersecurity class -. In Progress</a></li>
-                <li><a href="https://github.com/SarahYaw/PRNG">Here is a program of different PRNG algorithms researched and coded up for an Independent Study</a></li>
-                <li><a href="https://github.com/SarahYaw/CommNetProj4">Centralized Routing Via a Routing Matrix (Using Dijkstra)</a></li>
-                <li><a href="https://github.com/SarahYaw/OSProj5">Virtual Memory Simulation</a></li>
-                <li><a href="https://github.com/SarahYaw/OSProj4">OS simulator with I/O</a></li>
-                <li><a href="https://github.com/SarahYaw/CommNetProj3">Multithreaded client-server program with encryption</a></li>
-                <li><a href="https://github.com/SarahYaw/OSProj3">Simple scheduler simulator</a></li>
-                <li><a href="https://github.com/SarahYaw/CommNetProj2">Multithreaded client server project</a></li>
-                <li><a href="https://github.com/SarahYaw/OSProj2">Multithreading and synchronization project for Operating Systems class</a></li>
-            </ul>
-        </div>
-        <div>
-            <p>Previous Work</p>
-            <table>
-                <tr><td>Rock Solid Logistics</td><td>Brighton, MI</td><td>Web Design Intern</td><td>April, 2021 to August, 2021</td></tr>
-                <tr><td>Eastern Michigan University</td><td>Ypsilanti, MI</td><td>Graduate Assistant</td><td>September, 2020 to Present</td></tr>
-                <tr><td>Eastern Michigan University</td><td>Ypsilanti, MI</td><td>Teaching Assistant</td><td>January, 2020 to April, 2020</td></tr>
-                <tr><td>The Honors College at EMU</td><td>Ypsilanti, MI</td><td>Graphic Design Student Fellow</td><td>September, 2017 to April, 2020</td></tr>
-            </table><br/>
-        </div>
-    </>
+    <div>
+        <p style={subheader}>Education</p>
+        <table>
+            <tr><td>Bachelor of Science in Computer Science</td><td>April 2020, GPA: 3.74</td></tr><tr><td>Eastern Michigan University</td><td>Ypsilanti, Michigan</td></tr>
+            <tr><td>Master of Science, Computer Science</td><td>Expected December 2022</td></tr><tr><td>Eastern Michigan University</td><td>Ypsilanti, Michigan</td></tr>
+        </table>
+    </div>   
     );
-  }
-  export default Footer;
+}
+export const skills = () => {
+    const subheader = {
+        backgroundColor:"#98ABC8",
+        padding:"10px", 
+    };
+    return(
+    <div>
+        <p style={subheader}>Programming Proficiencies</p>
+        <ul>
+            <li>Adobe Photoshop, Illustrator, Indesign, Dreamweaver: 6 years</li>
+            <li>Lucidpress: 4 years</li>
+            <li>Canva: 4 years</li>
+            <li>HTML &amp; CSS: 6 years</li>
+            <li>Java: 6 years</li>
+            <li>JS: 3 years</li>
+            <li>MySQL &amp; NoSql: 1.5 years</li>
+            <li>Php: 1.5 years</li>
+            <li>Python: 1.5 years</li>
+        </ul>
+    </div>
+    );
+}
+export const projects = () => {
+    const subheader = {
+        backgroundColor:"#98ABC8",
+        padding:"10px", 
+    };
+    return(
+    <div>
+        <p style={subheader}>Past Projects</p>
+        <ul>
+            <li><a href="https://github.com/SarahYaw/CyberSec-HW2">Develop an application which has the options: RSA, Diffie-Hellman, and Elliptic curve cryptography - In Progress</a></li>
+            <li><a href="https://github.com/SarahYaw/592-PKI">Rough PKI built for Cybersecurity class -. In Progress</a></li>
+            <li><a href="https://github.com/SarahYaw/PRNG">Here is a program of different PRNG algorithms researched and coded up for an Independent Study</a></li>
+            <li><a href="https://github.com/SarahYaw/CommNetProj4">Centralized Routing Via a Routing Matrix (Using Dijkstra)</a></li>
+            <li><a href="https://github.com/SarahYaw/OSProj5">Virtual Memory Simulation</a></li>
+            <li><a href="https://github.com/SarahYaw/OSProj4">OS simulator with I/O</a></li>
+            <li><a href="https://github.com/SarahYaw/CommNetProj3">Multithreaded client-server program with encryption</a></li>
+            <li><a href="https://github.com/SarahYaw/OSProj3">Simple scheduler simulator</a></li>
+            <li><a href="https://github.com/SarahYaw/CommNetProj2">Multithreaded client server project</a></li>
+            <li><a href="https://github.com/SarahYaw/OSProj2">Multithreading and synchronization project for Operating Systems class</a></li>
+        </ul>
+      </div>
+    );
+}
+export const prevWork = () => {
+    const subheader = {
+        backgroundColor:"#98ABC8",
+        padding:"10px", 
+    };
+    return(
+    <div>
+        <p style={subheader}>Previous Work</p>
+        <table>
+            <tr><td>Rock Solid Logistics</td><td>Brighton, MI</td><td>Web Design Intern</td><td>April, 2021 to August, 2021</td></tr>
+            <tr><td>Eastern Michigan University</td><td>Ypsilanti, MI</td><td>Graduate Assistant</td><td>September, 2020 to Present</td></tr>
+            <tr><td>Eastern Michigan University</td><td>Ypsilanti, MI</td><td>Teaching Assistant</td><td>January, 2020 to April, 2020</td></tr>
+            <tr><td>The Honors College at EMU</td><td>Ypsilanti, MI</td><td>Graphic Design Student Fellow</td><td>September, 2017 to April, 2020</td></tr>
+        </table><br/>
+      </div>
+    );
+}
+export const intro = () => {
+    return(
+        <div>
+            <p>Hello! My name is Sarah Yaw. This is an intro/CV page made to become more familiar with ReactJS. I have a BS in Applied Computer Science from Eastern Michigan University and I'm a Masters Student and Grad Assistant at EMU currently.</p>
+        </div>
+    );
+}
